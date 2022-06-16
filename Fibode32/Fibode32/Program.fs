953669228daab4c42e3bf3bcdc8fbf0756969267
rtapiaf <91601943+rtapiaf@users.noreply.github.com>
2022-06-15 23:32:20 -0400

    Add files via upload

---
+++ Fibode32/Fibode32/Program.fs
@@ -0,0 +1,26 @@
+﻿// Más información acerca de F# en http://fsharp.net
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+open System
+
+let rec fibo n =
+    match n with
+    | 1 -> 0
+    | 2 -> 1
+    | 3 -> 1
+    | n -> fibo (n-1) + fibo (n-2) + fibo (n-3)
+
+let serie_fibo_recur n =
+    for i in 1..n do
+        printf "%d " (fibo i)
+        
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    Console.WriteLine("Ingrese un numero: ")
+    let n1 = Console.ReadLine()
+    let n = Convert.ToInt32(n1)
+    //let n = Console.ReadKey()
+    //Console.WriteLine(fibo n)
+    serie_fibo_recur n
+    let tecla = Console.ReadKey()
+    0 // devolver un código de salida entero
